acb1156f866d1a957df43f80777f08b0e07b9365
tiagoquin <tiago.quin@gmail.com>
2019-03-11 16:50:50 +0100

    un test passe dans dfs explorer o/

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -9,14 +9,23 @@ import java.io.File;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+  public void explore(File rootDirectory, IFileVisitor visitor) {
+      visitor.visit(rootDirectory);
 
+      if (rootDirectory.isDirectory()) {
+          for (File fichiers: rootDirectory.listFiles()) {
+              if (fichiers.isDirectory()) {
+                  explore(fichiers, visitor);
+              } else {
+                  visitor.visit(fichiers);
+              }
+          }
+      }
+  }
 }
